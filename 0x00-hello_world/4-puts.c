63deec62a68ea2a6ae8a7feffea78958f975acfc
ThembelaXelelo <meekbella70@gmail.com>
2023-03-24 06:39:15 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
-#include<studio.h
+#include<stdio.h>
 /**
  * main - Entry point
  * 
- * Return: Always 0 {Sucess}
+ * Return: Always 0 (Sucess)
  */
-int mainO(void)
+int main(void)
 {
-	puts ("\""Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
