980589910f2f025f1cd75bdeadb5d589439b8a1d
ThembelaXelelo <meekbella70@gmail.com>
2023-03-24 06:23:22 -0700

    All files are executable

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<studio.h
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 {Sucess}
+ */
+int mainO(void)
+{
+	puts ("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include<studio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Sucess)
+ * */
+int main(void)
+{
+	printf("With proper grammer, but the outcome is a piece of art ,\n");
+	return (0);
+}
+
+		
